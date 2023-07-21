f5082243bd116e9ab98d51063b6e50031935ebae
ChataOfc <122675177+ChataOfc@users.noreply.github.com>
2023-07-21 10:18:06 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const list = document.querySelectorAll('.list');
+    function activeLink(){
+      list.forEach((item) =>
+      item.classList.remove('active'));
+      this.classList.add('active');
+    }
+    list.forEach((item) =>
+    item.addEventListener('click'.activeLink));
+
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
